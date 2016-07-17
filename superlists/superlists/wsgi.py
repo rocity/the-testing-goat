f7ac62fe8021d84c1cf2955f3e52801fecca2182
rocity <hello@kvnpl.xyz>
2016-07-17 12:40:35 +0800

    Modify wsgi file for staging server

--- superlists/superlists/wsgi.py
+++ superlists/superlists/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+path = '/home/rocity/sites/the-testing-goat/superlists'
+    if path not in sys.path:
+        sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
