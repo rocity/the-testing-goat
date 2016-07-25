54807363ea1ce15713f4d91b89407006145d2c55
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-07-25 09:54:27 +0800

    Use form on all views

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -26,6 +26,30 @@ class ListViewTest(TestCase):
     def tearDown(self):
         pass
 
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'text': ''}
+            )
+
+    def test_for_invalid_input_nothing_saved_to_db(self):
+        self.post_invalid_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+    def text_for_invalid_input_renders_list_template(self):
+        response = self.post_invalid_input()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lists/list.html')
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_for_invalid_input_shows_error_on_page(self):
+        response = self.post_invalid_input()
+        self.assertContains(response, escape(EMPTY_LIST_ERROR))
+
     def test_uses_list_template(self):
         list_ = List.objects.create()
 
